ce84604f99f694c62378cbc36954694b6c7c80e5
Devendra Gurjar <dev.viga@gmail.com>
2024-11-16 02:37:03 +0530

    fix: button,wallpaper, logo

--- src/loader/Loader.jsx
+++ src/loader/Loader.jsx
@@ -2,13 +2,13 @@ import "./PageLoader.css";
 
 const Loader = () => {
   return (
-    <div id="loader" className="loader_page">
+    <div className="loader_page">
       <div
         style={{ display: "flex", margin: "auto", justifyContent: "center" }}
         className="page-loader-main"
       >
         {/* <Stars number={2000} /> */}
-        <div className="hexagon" aria-label="Animated hexagonal ripples">
+        <div id="loader" className="hexagon" aria-label="Animated hexagonal ripples">
           <div className="hexagon__group">
             <div className="hexagon__sector"></div>
             <div className="hexagon__sector"></div>
