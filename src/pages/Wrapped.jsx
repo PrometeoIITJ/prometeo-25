ef611cbd22abd71a158e820de05f59828e47e515
a19hu <ashutoshkumar1jkl@gmail.com>
2024-12-05 22:14:48 +0530

    feat:wrapped page add

---
+++ src/pages/Wrapped.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './wrapped.css'
+import logo from '../assets/coming-soon/Frame_10.svg'
+import { Link } from "react-router-dom";
+import { div } from 'framer-motion/client';
+
+export default function () {
+  return (
+    <div style={{height:"100%",width:"100%"}}>
+    <div className='wrapped_container'>
+    <div className="logo">
+                <Link to="/"><img src={logo} alt="Logo" /></Link>
+              </div>
+    <div className='wrapped_box'>
+        
+        <div className='text_'>
+        Prometeo’24 is wrapped <br /> we’re coming soon!
+        </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+ 
